030fa8439c4bb5c41b9294ce919139edbe2c3a62
JutilaO <oskari.jutila01@gmail.com>
2024-11-28 13:38:21 +0200

    Lisätty kartta

--- game.js
+++ game.js
@@ -18,3 +18,32 @@ function start() {
         //document.getElementById('login_panel').hidden = true
     }
 }
+
+let map;
+function mapPosition(latitude, longitude, markerText) {
+    if(!map) {
+        map = L.map('map').setView([latitude, longitude], 13)
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }).addTo(map)
+    } else {
+        map.flyTo([latitude, longitude], 13, {
+            animate: true,
+            duration: 5,
+            easeLinearity: 0.25
+        })
+    }
+    if(markerText) {
+        L.marker([latitude, longitude]).addTo(map)
+            .bindPopup(markerText)
+            .openPopup()
+    }
+}
+
+//Asettaa kartan Helsinki-Vantaan lentokentälle
+mapPosition(60.315369447382345, 24.945276215177994)
+
+//Testilento
+setTimeout(() => {
+    mapPosition(30, 30, "katti")
+}, 5000)
