4012abf55f50316772c23fc93fd2ecbef1a49580
Mikko Ilvonen <mikkoilvonen94@gmail.com>
2024-12-05 11:22:16 +0200

    lisäsin sään vaikuttamaan pisteisiin.

--- backend_main.py
+++ backend_main.py
@@ -6,13 +6,13 @@ from geopy import distance
 import requests
 
 
-
 def get_start_location():
     sql = f'SELECT ident, continent, latitude_deg, longitude_deg FROM airport WHERE ident in(SELECT location FROM game);'
     cursor = connection.cursor()
     cursor.execute(sql)
     start_location = cursor.fetchall()
     return start_location[0]
+
 
 class Player:
 
@@ -27,8 +27,9 @@ class Player:
         self.distance = distance
         self.points = points
 
-    def get_random_location(self,deg, min, max):
-        sql = (f'SELECT airport.ident, airport.name AS airport, country.name AS country, airport.continent, airport.latitude_deg, airport.longitude_deg FROM airport JOIN country ON airport.iso_country = country.iso_country WHERE {deg} > %s AND {deg} < %s ORDER BY RAND() LIMIT 1;')
+    def get_random_location(self, deg, min, max):
+        sql = (
+            f'SELECT airport.ident, airport.name AS airport, country.name AS country, airport.continent, airport.latitude_deg, airport.longitude_deg FROM airport JOIN country ON airport.iso_country = country.iso_country WHERE {deg} > %s AND {deg} < %s ORDER BY RAND() LIMIT 1;')
         cursor = self.connection.cursor(dictionary=True)
         cursor.execute(sql, (min, max))
         random_location = cursor.fetchall()
@@ -44,10 +45,14 @@ class Player:
         return random_location
 
     def get_5_locations(self):
-        location_north = self.get_random_location("latitude_deg", str(float(self.latitude) + 10), str(float(self.latitude) + 20))
-        location_south = self.get_random_location("latitude_deg", str(float(self.latitude) - 20), str(float(self.latitude) - 10))
-        location_east = self.get_random_location("longitude_deg", str(float(self.longitude) + 10), str(float(self.longitude) + 20))
-        location_west = self.get_random_location("longitude_deg", str(float(self.longitude) - 20), str(float(self.longitude) - 10))
+        location_north = self.get_random_location("latitude_deg", str(float(self.latitude) + 10),
+                                                  str(float(self.latitude) + 20))
+        location_south = self.get_random_location("latitude_deg", str(float(self.latitude) - 20),
+                                                  str(float(self.latitude) - 10))
+        location_east = self.get_random_location("longitude_deg", str(float(self.longitude) + 10),
+                                                 str(float(self.longitude) + 20))
+        location_west = self.get_random_location("longitude_deg", str(float(self.longitude) - 20),
+                                                 str(float(self.longitude) - 10))
         location_same_continent = self.get_location_on_same_continent()
         locations = [location_north, location_south, location_east, location_west, location_same_continent]
         return locations
@@ -75,17 +80,27 @@ class Player:
         self.points += points
 
     def get_location_weather(self):
-        haku = "https://api.openweathermap.org/data/2.5/weather?lat=" + str(self.latitude) + "&lon=" + str(self.longitude) + "&appid=" + str(apikey) + "&units=metric&lang=Fi"
+        haku = "https://api.openweathermap.org/data/2.5/weather?lat=" + str(self.latitude) + "&lon=" + str(
+            self.longitude) + "&appid=" + str(apikey) + "&units=metric&lang=Fi"
         json_weather = None
         try:
             vastaus = requests.get(haku)
             if vastaus.status_code == 200:
                 vast = vastaus.json()
-                json_weather = vast["weather"][0]["description"]
+                description = vast["weather"][0]["description"]
+                weather_id = vast["weather"][0]["id"]
+                json_weather = {"description": description, "id": weather_id}
         except requests.exceptions.RequestException as e:
             json_weather = {}
             print(e)
         return json_weather
+
+    def get_weather_points(self, weather_id):
+        sql = f'SELECT points FROM weathers where ID = %s;'
+        cursor = self.connection.cursor()
+        cursor.execute(sql, (weather_id,))
+        points = cursor.fetchone()
+        return points[0] if points else 0
 
     def get_random_event(self):
         sql = "SELECT text, points FROM random_events ORDER BY RAND() LIMIT 1;"
@@ -100,6 +115,7 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 players = []
 
+
 @app.route("/player_name/<name>")
 def get_player_name(name):
     start_location = get_start_location()
@@ -111,6 +127,7 @@ def get_player_name(name):
     jsonvast = json.dumps(vastaus)
     return Response(response=jsonvast, status=status, mimetype="application/json")
 
+
 @app.route("/new_location/<ident>")
 def new_location(ident):
     pelaaja = players[-1]
@@ -118,9 +135,21 @@ def new_location(ident):
     pelaaja.distance += distance_to
     pelaaja.set_new_location(ident)
     weather = pelaaja.get_location_weather()
+    weather_points = 0
+    if weather != None:
+        weather_points = pelaaja.get_weather_points(weather['id'])
+        return weather_points
     event = pelaaja.get_random_event()
-    pelaaja.adjust_points(event["points"])
-    vastaus = weather, event["string"], pelaaja.points, pelaaja.distance
+    point_change = weather_points + event["points"]
+    pelaaja.adjust_points(point_change)
+    vastaus = {
+        "weather": weather,
+        "event_string": event["string"],
+        "point_change": point_change,
+        "player_points": pelaaja.points,
+        "player_distance": pelaaja.distance
+    }
+
     status = 200
     jsonvast = json.dumps(vastaus)
     return Response(response=jsonvast, status=status, mimetype="application/json")
@@ -129,52 +158,54 @@ def new_location(ident):
 # Palauttaa tarinan tekstin:
 @app.route('/story')
 def get_story():
-      story = {
-            'story_text' : 'Oletko valmis lähtemään ennennäkemättömälle matkalle?<br>'
-                           '<br>'
-                           'Olet jo kauan suunnnittelu tekeväsi aivan ennennäkemättömän lomakiertomatkan<br>'
-                           'ymprärin maailmaa, mutta olet ollut huolissasi lentämisen vaikutuksista ympäristölle.<br>'
-                           'Lentotekniikka on tulevaisuudessa kehittynyt niin paljon, ettei lentämisen vaikutukset<br>'
-                           'ympäristölle ole enää yhtä voimakkaita. Aiot nyt toteuttaa suunnitelmasi.<br>'
-                           '<br>'
-                           'Haluat matkasi olevan ikimuistoinen, sillä et ole varma, voitko lähteä kiertomatkalle<br>'
-                           'enää toiste. Aioit kerätä uniikkeja ja  merkityksellisiä matkamuistoja kohteista,<br>'
-                           'tutustua paikallisiin nähtävyyksiin sekä kokea mahdollisimman paljon.<br>'
-                           '<br>'
-                           'Kestävä kehitys on sinulle tärkeää. Haluat matkasi olevan kestävän kehityksen näkökulmasta<br>'
-                           'mahdollisimman kannattava, mutta tiedostat, ettei se ole realistinen tavoite, kun ottaa huomioon<br>'
-                           'kuluttamisen ja lentämisen vaikutukset ympäristölle. Varaudut siihen, miten tämä tulee<br>'
-                           'varjostamaan matkaasi.<br>'
-                           '<br>'
-                           'Pakkaa matkalaukkusi, sillä ensimmäinen lentosi odottaa!<br>'
-                           '<br>'
-                           'Maailma, täältä tullaan!'
-      }
-      return story
+    story = {
+        'story_text': 'Oletko valmis lähtemään ennennäkemättömälle matkalle?<br>'
+                      '<br>'
+                      'Olet jo kauan suunnnittelu tekeväsi aivan ennennäkemättömän lomakiertomatkan<br>'
+                      'ymprärin maailmaa, mutta olet ollut huolissasi lentämisen vaikutuksista ympäristölle.<br>'
+                      'Lentotekniikka on tulevaisuudessa kehittynyt niin paljon, ettei lentämisen vaikutukset<br>'
+                      'ympäristölle ole enää yhtä voimakkaita. Aiot nyt toteuttaa suunnitelmasi.<br>'
+                      '<br>'
+                      'Haluat matkasi olevan ikimuistoinen, sillä et ole varma, voitko lähteä kiertomatkalle<br>'
+                      'enää toiste. Aioit kerätä uniikkeja ja  merkityksellisiä matkamuistoja kohteista,<br>'
+                      'tutustua paikallisiin nähtävyyksiin sekä kokea mahdollisimman paljon.<br>'
+                      '<br>'
+                      'Kestävä kehitys on sinulle tärkeää. Haluat matkasi olevan kestävän kehityksen näkökulmasta<br>'
+                      'mahdollisimman kannattava, mutta tiedostat, ettei se ole realistinen tavoite, kun ottaa huomioon<br>'
+                      'kuluttamisen ja lentämisen vaikutukset ympäristölle. Varaudut siihen, miten tämä tulee<br>'
+                      'varjostamaan matkaasi.<br>'
+                      '<br>'
+                      'Pakkaa matkalaukkusi, sillä ensimmäinen lentosi odottaa!<br>'
+                      '<br>'
+                      'Maailma, täältä tullaan!'
+    }
+    return story
+
 
 # Palauttaa ohjeet tekstinä:
 @app.route('/instructions')
 def get_instructions():
     instructions = {
-        "instructions_text" : "Tavoitteenasi on kerätä seitsemän (7) matkamuistoa ja <br>"
-                              "mahdollisimman paljon pisteitä matkustelemalla eri kohteisiin.<br>"
-                              "<br>"
-                              "Koet ja näet matkan aikana kaikenlaista, ja riippuen kokemuksesta,<br>"
-                              "voit joko saada tai menettää pisteitä. Myös matkakohteen säällä on<br>"
-                              "vaikutus pisteisiisi.<br> "
-                              "<br>"
-                              "Jokaisessa kohteessa sinun on mahdollista kerätä matkamuisto,<br>"
-                              "mutta tämä ei ole pakollista, eli voit jatkaa matkaa seuraavaan kohteeseen<br>"
-                              "keräämättä matkamuistoa.<br>"
-                              "Matkamuistoista saa sitä enemmän pisteitä mitä kauempana olet kotoa.<br>"
-                              "<br>"
-                              "Muista kuitenkin, että matkan kokonaispituus vaikuttaa negatiivisesti sinun lopullisiin<br>"
-                              "pisteisiin. Mitä enemmän kilometrejä keräsit, sitä enemmän pisteitä menetät.<br>"
-                              "<br>"
-                              "Palaat kotiin, kun olet kerännyt seitsemän (7) matkamuistoa.<br>"
-                              "<br>Onnea matkaan!"
+        "instructions_text": "Tavoitteenasi on kerätä seitsemän (7) matkamuistoa ja <br>"
+                             "mahdollisimman paljon pisteitä matkustelemalla eri kohteisiin.<br>"
+                             "<br>"
+                             "Koet ja näet matkan aikana kaikenlaista, ja riippuen kokemuksesta,<br>"
+                             "voit joko saada tai menettää pisteitä. Myös matkakohteen säällä on<br>"
+                             "vaikutus pisteisiisi.<br> "
+                             "<br>"
+                             "Jokaisessa kohteessa sinun on mahdollista kerätä matkamuisto,<br>"
+                             "mutta tämä ei ole pakollista, eli voit jatkaa matkaa seuraavaan kohteeseen<br>"
+                             "keräämättä matkamuistoa.<br>"
+                             "Matkamuistoista saa sitä enemmän pisteitä mitä kauempana olet kotoa.<br>"
+                             "<br>"
+                             "Muista kuitenkin, että matkan kokonaispituus vaikuttaa negatiivisesti sinun lopullisiin<br>"
+                             "pisteisiin. Mitä enemmän kilometrejä keräsit, sitä enemmän pisteitä menetät.<br>"
+                             "<br>"
+                             "Palaat kotiin, kun olet kerännyt seitsemän (7) matkamuistoa.<br>"
+                             "<br>Onnea matkaan!"
     }
     return instructions
+
 
 # Laskee matkamuiston pistearvon
 # aloituslentokentän ja tämän hetkisen lentokentän välisen etäisyyden avulla. Päivittää lasketun pistearvon tietokantaan.
@@ -192,7 +223,7 @@ def calculate_trophy_points_and_update_points():
     start_lat_long = cursor.fetchall()
 
     start_lat, start_long = start_lat_long[0]
-    current_lat_long = (players[0].latitude, players[0].longitude)
+    current_lat_long = (players[-1].latitude, players[-1].longitude)
     start_to_current_distance = distance.distance(
         (start_lat, start_long),
         (current_lat_long[0], current_lat_long[1])
@@ -200,11 +231,11 @@ def calculate_trophy_points_and_update_points():
     trophy_points = 30 + int(start_to_current_distance / 125)
 
     # Päivittää pisteet:
-    players[0].points = players[0].points + trophy_points
+    players[-1].points = players[-1].points + trophy_points
     # print(players[0].points)
 
     # Päivittää tämänhetkisten matkamuistojen määrän:
-    players[0].trophys = players[0].trophys + 1
+    players[-1].trophys = players[-1].trophys + 1
     # print(players[0].trophys)
 
     response = {
@@ -235,7 +266,6 @@ def check_trophy_status():
     return response
 
 
-
 @app.route("/5airports")
 def get_options():
     pelaaja = players[-1]
@@ -243,6 +273,7 @@ def get_options():
     jsonvast = json.dumps(vastaus)
     status = 200
     return Response(response=jsonvast, status=status, mimetype="application/json")
+
 
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=3000)
